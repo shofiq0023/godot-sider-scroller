93e279c70d0b2c9a307a068eb68dea0a9b505bb4
shofiq0023 <shofiqulislam0023@gmail.com>
2022-05-19 10:33:33 +0600

    Added walk animation and logic

--- Assets/Characters/Player/Player.gd
+++ Assets/Characters/Player/Player.gd
@@ -1,21 +1,45 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO;
-var title = "Game v1"
+var walking = false;
 
 const moveSpeed = 200;
+const walkSpeed = 100;
 const GRAVITY = 20;
 const JUMPFORCE = -400;
 
 func _ready():
 	pass
 
+# Gets input with input(type -> "run" or "walk") and direction ("left" of "right")
+func input(input, dir):
+	if input == "run" and dir == "right":
+		velocity.x = moveSpeed;
+	if input == "run" and dir == "left":
+		velocity.x = -moveSpeed;
+	if input == "walk" and dir == "right":
+		velocity.x = walkSpeed;
+	if input == "walk" and dir == "left":
+		velocity.x = -walkSpeed;
+		
+	if dir == "right":
+		$AnimatedSprite.flip_h = false;
+	elif dir == "left":
+		$AnimatedSprite.flip_h = true;
+	
+	getAnimation();
+
+# Plays animation
 func getAnimation():
 	if is_on_floor():
 		if velocity.x == 0:
 			$AnimatedSprite.play("Idle");
-		if (velocity.x > 0) or (velocity.x < 0):
-			$AnimatedSprite.play("Running");
+		if walking == false:
+			if (velocity.x > 0) or (velocity.x < 0):
+				$AnimatedSprite.play("Running");
+		if walking == true:
+			if (velocity.x > 0) or (velocity.x < 0):
+				$AnimatedSprite.play("Walking");
 	
 	if not is_on_floor():
 		if velocity.y < 0:
@@ -23,25 +47,37 @@ func getAnimation():
 		if velocity.y > 0:
 			$AnimatedSprite.play("Falling")
 
+# Gets user input
 func getInput():
-	if Input.is_action_pressed("right"):
-		velocity.x = moveSpeed;
-		getAnimation();
-		$AnimatedSprite.flip_h = false;
-	elif Input.is_action_pressed("left"):
-		velocity.x = -moveSpeed;
-		getAnimation();
-		$AnimatedSprite.flip_h = true;
-	else:
-		velocity.x = 0;
-		getAnimation();
+	if walking == false:
+		if Input.is_action_pressed("right"):
+			input("run", "right");
+		elif Input.is_action_pressed("left"):
+			input("run", "left");
+		else:
+			velocity.x = 0;
+			getAnimation();
+	elif walking == true:
+		if Input.is_action_pressed("right"):
+			input("walk", "right");
+		elif Input.is_action_pressed("left"):
+			input("walk", "left");
+		else:
+			velocity.x = 0;
+			getAnimation();
+	
+	# Toggle walk (CapsLock)
+	if Input.is_action_just_pressed("walk"):
+		walking = !walking;
+	
+	# Can jump if only on floor (means no double jump)
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMPFORCE;
 			getAnimation();
 
+# Main game function
 func _process(delta):
-	OS.set_window_title(title + " | fps: " + str(Engine.get_frames_per_second()));
 	velocity.y += GRAVITY;
 	
 	getInput();
